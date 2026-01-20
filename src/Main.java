376cb8204859657f37a6aa0a822be36ef4a39c67
rafaelmsantos28 <rafaelmedeirostrab@gmail.com>
2026-01-20 17:34:55 -0300

    interligando classes de dominio

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.lang.management.MemoryNotificationInfo;
@@ -25,9 +27,41 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
-        IO.println(curso);
-        IO.println(curso2);
-        IO.println(mentoria);
+//        IO.println(curso);
+//        IO.println(curso2);
+//        IO.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devRafael = new Dev();
+        devRafael.setNome("Rafael");
+        devRafael.inscreverBootcamp(bootcamp);
+        IO.println("Conteúdos Inscritos: " + devRafael.getConteudosInscritos() );
+        devRafael.progredir();
+        devRafael.progredir();
+        IO.println("Conteúdos Inscritos: " + devRafael.getConteudosInscritos() );
+        IO.println("Conteúdos Concluidos: " + devRafael.getConteudosConcluidos() );
+        IO.println("XP: " + devRafael.calcularTotalXp());
+
+        IO.println("------------------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        IO.println("Conteúdos Inscritos: " + devJoao.getConteudosInscritos() );
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        IO.println("Conteúdos Inscritos: " + devJoao.getConteudosInscritos() );
+        IO.println("Conteúdos Concluidos: " + devJoao.getConteudosConcluidos() );
+        IO.println("XP: " + devJoao.calcularTotalXp());
+
+
 
 
     }
